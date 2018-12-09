beb7a577322098bdbd86dd3e570e764919bd8945
Tom Wadeson <tom@tomwadeson.com>
2018-12-09 19:23:08 +0000

    Polymorhpic `WorkerPool`

--- src/main/scala/exercises/WorkerPool.scala
+++ src/main/scala/exercises/WorkerPool.scala
@@ -1,64 +1,65 @@
 package exercises
 
 import cats.effect.concurrent.{MVar, Ref}
-import cats.effect.{ExitCode, IO, IOApp, Timer}
+import cats.effect._
 import cats.implicits._
+import cats.effect.implicits._
 
 import scala.concurrent.duration._
 import scala.util.Random
 
 object WorkerPool extends IOApp {
 
-  type Worker[A, B] = A => IO[B]
+  type Worker[F[_], A, B] = A => F[B]
 
-  def mkWorker(id: Int)(implicit timer: Timer[IO]): IO[Worker[Int, Int]] =
-    Ref[IO].of(0).map { counter =>
-      def simulateWork: IO[Unit] =
-        IO(50 + Random.nextInt(450)).map(_.millis).flatMap(IO.sleep)
+  def mkWorker[F[_]](id: Int)(implicit F: Sync[F], timer: Timer[F]): F[Worker[F, Int, Int]] =
+    Ref[F].of(0).map { counter =>
+      def simulateWork: F[Unit] =
+        F.delay(50 + Random.nextInt(450)).map(_.millis).flatMap(timer.sleep)
 
-      def report: IO[Unit] =
-        counter.get.flatMap(i => IO(println(s"Total processed by $id: $i")))
+      def report: F[Unit] =
+        counter.get.flatMap(i => F.delay(println(s"Total processed by $id: $i")))
 
       x =>
         simulateWork >>
           counter.update(_ + 1) >>
           report >>
-          IO.pure(x + 1)
+          F.pure(x + 1)
     }
 
-  trait WorkerPool[A, B] {
-    def exec(a: A): IO[B]
+  trait WorkerPool[F[_], A, B] {
+    def exec(a: A): F[B]
   }
 
   object WorkerPool {
 
-    def of[A, B](fs: List[Worker[A, B]]): IO[WorkerPool[A, B]] = {
+    def of[F[_] : Concurrent, A, B](workers: List[Worker[F, A, B]]): F[WorkerPool[F, A, B]] = {
 
-      def adaptWorker(worker: Worker[A, B], workerChannel: MVar[IO, Worker[A, B]]): Worker[A, B] = {
-        lazy val adapted: Worker[A, B] =
+      def adaptWorker(worker: Worker[F, A, B], workerChannel: MVar[F, Worker[F, A, B]]): Worker[F, A, B] = {
+        lazy val adapted: Worker[F, A, B] =
           a => worker(a) <* workerChannel.put(adapted).start
 
         adapted
       }
 
       for {
-        workerChannel <- MVar.empty[IO, Worker[A, B]]
-        _ <- fs.traverse_(worker => workerChannel.put(adaptWorker(worker, workerChannel)).start)
-      } yield new WorkerPool[A, B] {
-        override def exec(a: A): IO[B] =
+        workerChannel <- MVar.empty[F, Worker[F, A, B]]
+        _ <- workers.traverse_(worker => workerChannel.put(adaptWorker(worker, workerChannel)).start)
+      } yield new WorkerPool[F, A, B] {
+        override def exec(a: A): F[B] =
           workerChannel.take.flatMap(worker => worker(a))
       }
     }
   }
 
-  val testPool: IO[WorkerPool[Int, Int]] =
+  val testPool: IO[WorkerPool[IO, Int, Int]] =
     List.range(0, 10)
-      .traverse(mkWorker)
-      .flatMap(WorkerPool.of)
+      .traverse(mkWorker[IO])
+      .flatMap(WorkerPool.of[IO, Int, Int])
 
   override def run(args: List[String]): IO[ExitCode] =
     for {
       workerPool <- testPool
-      _ <- List.range(0, 100).parTraverse_(workerPool.exec)
+      _ <- List.range(0, 50).parTraverse_(workerPool.exec)
     } yield ExitCode.Success
 }
